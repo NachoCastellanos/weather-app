7d442fc92be90382ca76d837588c2c6fa9ffe30d
Nacho Castellanos Moreno <nacho2casmor@gmail.com>
2024-07-09 16:45:21 +0200

    Remove unusued classes

--- weather-app-backend/weather-app-backend/src/main/java/com/example/weather_app_backend/model/prediccion/DatosMeteorologicos.java
+++
@@ -1,8 +0,0 @@
-package com.example.weather_app_backend.model.prediccion;
-
-public interface DatosMeteorologicos {
-    String getValue();
-    void setValue(String value);
-    String getPeriodo();
-    void setPeriodo(String periodo);
-}
